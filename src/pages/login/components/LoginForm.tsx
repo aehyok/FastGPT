0e242bca8227a766e2c3fac3046c72d51dc226e2
刘启明 <455043818@qq.com>
2023-06-16 17:09:02 +0800

    chore: comment  login

--- src/pages/login/components/LoginForm.tsx
+++ src/pages/login/components/LoginForm.tsx
@@ -97,7 +97,7 @@ const LoginForm = ({ setPageType, loginSuccess }: Props) => {
             {!!errors.password && errors.password.message}
           </FormErrorMessage>
         </FormControl>
-        {/* <Flex align={'center'} justifyContent={'space-between'} mt={6} color={'myBlue.600'}>
+        <Flex align={'center'} justifyContent={'space-between'} mt={6} color={'myBlue.600'}>
           <Box
             cursor={'pointer'}
             _hover={{ textDecoration: 'underline' }}
@@ -114,7 +114,7 @@ const LoginForm = ({ setPageType, loginSuccess }: Props) => {
           >
             注册账号
           </Box>
-        </Flex> */}
+        </Flex>
         <Button
           type="submit"
           mt={8}
