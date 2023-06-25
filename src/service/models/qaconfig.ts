6dae3bae60502a2aafee0af28eaf95e9b714f9c9
刘启明 <455043818@qq.com>
2023-06-25 18:29:09 +0800

    feat: add qaconfig api

---
+++ src/service/models/qaconfig.ts
@@ -0,0 +1,25 @@
+import { Schema, model, models, Model as MongoModel } from 'mongoose';
+import { ModelSchema as ModelType } from '@/types/mongoSchema';
+
+const QaConfigSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    required: true
+  },
+  code: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  prompt: {
+    type: String,
+    required: true
+  }
+});
+
+export const QaConfig: MongoModel<ModelType> =
+  models['qaconfig'] || model('qaconfig', QaConfigSchema);
